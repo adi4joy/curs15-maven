70aedf378e363f7955a936fe20ae9deb7981721c
Adi Strete <adi4joy@yahoo.com>
2020-03-17 10:56:28 +0200

    Curs 15

--- src/main/java/ro/fasttrackit/mvnbase/HelloMaven.java
+++ src/main/java/ro/fasttrackit/mvnbase/HelloMaven.java
@@ -1,9 +1,15 @@
 package ro.fasttrackit.mvnbase;
 
+import com.github.lalyos.jfiglet.FigletFont;
+
+import java.io.IOException;
+
 public class HelloMaven {
 
-    public static void main(String[] args) {
-        System.out.println(new HelloMaven().sayHello());
+    public static void main(String[] args) throws IOException {
+        String hello = new HelloMaven().sayHello();
+        String asciiArt = FigletFont.convertOneLine(hello);
+        System.out.println(asciiArt);
     }
 
     public String sayHello() {
